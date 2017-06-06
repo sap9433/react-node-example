9aaa5648055b34839e20e9c07fe0d085bbe6e71e
Saptarshi Chatterjee <saptarshi.chatterjee@paytm.com>
2017-06-06 16:25:47 +0530

    All working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import _ from 'lodash';
+import moment from 'moment';
 
 import '../assets/stylesheets/base.scss';
 
@@ -27,18 +28,27 @@ export class App extends Component {
 
   render() {
       const { weather } = this.state;
-      return ( weather.list ? < div > 
+      const city = weather.city || {};
+      return ( weather.list ? < div className="wrapper">
+      	<div className="head"> Weather report for location lat/lan {city.coord.lat} / {city.coord.lon}</div> 
         {
-            weather.list.map((res, val) => {
+            weather.list.reverse().map((res, val) => {
 
               return ( < div className='eachAddressBlock'>
-                < div className='header'> { res.weather[0].description } < /div>
-                { _.map(res.main, (val , key) => {
-                	return < div className='description'> {key} , {val} < /div > 
-                })
+                < div className='header'> Measurement time - { moment(res.dt_txt).format('DD MMM YYYY HH:MM A') } < /div>
+                <div className="main"> Summery - {res.weather[0].description} . Main weather data Points - </div> 
+                { 
+                	_.map(res.main, (val , key) => {
+                		return < div className='description'> {key} : {val} < /div > 
+                	})
 
                 }
-                   
+                <div className="main"> Wind details data Points</div> 
+                   {
+	                   	_.map(res.wind, (val , key) => {
+	                		return < div className='description'> {key} : {val} < /div > 
+	                	})
+                	}
                  < /div>
               );
             })
